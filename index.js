499148e3e368a9e1cd5c6e93a8f9357cfde53b2b
Marcos Nascimento <marcos.nascimento@macys.com>
2015-07-13 22:33:49 -0300

    change port to default

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 5000))
+
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/home.html'));
@@ -32,4 +34,6 @@ app.use('/css', express.static(path.join(__dirname, '/public/css')));
 app.use('/js', express.static(path.join(__dirname, '/public/js')));
 app.use('/fonts', express.static(path.join(__dirname, '/public/fonts')));
 
-app.listen(80);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+});
