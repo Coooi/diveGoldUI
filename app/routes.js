4b8007800f44b0b1f52ac9c7159439f6d92ed452
Marco Nascimento (Coi) <coi182@gmail.com>
2015-10-05 15:08:14 -0300

    routing fix

--- app/routes.js
+++ app/routes.js
@@ -19,7 +19,7 @@ module.exports = function(app, passport) {
         res.sendFile(path.join(__dirname, '../public', 'reservas.html'));
     });
 
-    app.get('/confirmacoes', function(req, res) {
+    app.get('/confirmacoes', isLoggedIn, function(req, res) {
         res.sendFile(path.join(__dirname, '../public', 'confirmacoes.html'));
     });
 
